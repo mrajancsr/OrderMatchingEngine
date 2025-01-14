7028b6ac1180cc086dc3d10cb7f779a4004d16c1
mrajancsr <mrajancsr@gmail.com>
2025-01-13 19:30:23 -0500

    added modifyOrders, will need to change in future based on order matching logic

--- OrderEngine.h
+++ OrderEngine.h
@@ -15,6 +15,7 @@ private:
     std::string m_user;
     std::string m_company;
     unsigned int m_qty;
+    double m_price;
 
 public:
     Order(const std::string &ordId,
@@ -22,20 +23,23 @@ public:
           const std::string &side,
           const unsigned int qty,
           const std::string &user,
-          const std::string &company)
-        : m_orderId{ordId}, m_securityId{secId}, m_side{side}, m_qty{qty}, m_user{user}, m_company{company} {}
+          const std::string &company,
+          const double &price = 0.0)
+        : m_orderId{ordId}, m_securityId{secId}, m_side{side}, m_qty{qty}, m_user{user}, m_company{company}, m_price{price} {}
 
     std::string orderId() const { return m_orderId; }
     std::string securityId() const { return m_securityId; }
     unsigned int qty() const { return m_qty; }
-    std::string userId() const { return m_user; }
+    std::string user() const { return m_user; }
     std::string side() const { return m_side; }
+    std::string company() const { return m_company; }
+    double price() const { return m_price; }
 
     bool operator==(const Order &otherOrder) const
     {
         return (this->m_orderId == otherOrder.orderId() &&
                 (this->securityId() == otherOrder.securityId()) &&
-                (this->userId() == otherOrder.userId()) &&
+                (this->user() == otherOrder.user()) &&
                 (this->side() == otherOrder.side()));
     }
 };
@@ -49,7 +53,7 @@ namespace std
         {
             return std::hash<std::string>()(order.orderId()) ^
                    (std::hash<std::string>()(order.securityId()) << 1) ^
-                   (std::hash<std::string>()(order.userId()) << 2);
+                   (std::hash<std::string>()(order.user()) << 2);
         }
     };
 }
@@ -59,9 +63,22 @@ class IOrderEngine
 public:
     virtual void addOrder(const Order &order) = 0;
     virtual void cancelOrder(const std::string &orderId) = 0;
-    virtual std::vector<Order> getOrder(const std::string &orderId) = 0;
+    virtual std::optional<Order> getOrder(const std::string &orderId) const = 0;
     virtual void cancelOrderByUser(const std::string &userId) = 0;
-    virtual std::unordered_set<Order> getOrdersByUser(const std::string &userId) = 0;
+
+    // get all orders for this particular user
+    virtual std::vector<Order> getOrdersByUser(const std::string &userId) const = 0;
+
+    // get all active orders in the engine
+    virtual std::vector<Order> getAllOrders() const = 0;
+
+    // remove all orders in engine for this security with qty >= minQty
+    virtual void cancelOrdersForSecIdWithMinimumQty(const std::string &secId, unsigned int minQty) = 0;
+
+    // return the total qty that can match for the security id
+    virtual unsigned int getMatchingSizeForSecurity(const std::string &securityId) = 0;
+
+    virtual void modifyOrder(const std::string &orderId, const unsigned int &newQty) = 0;
 };
 
 class OrderEngine : public IOrderEngine
@@ -81,27 +98,128 @@ public:
         if (!inserted)
             throw ::std::runtime_error("Duplicate order detected: " + order.orderId());
         m_ordersBySecurityId[order.securityId()].insert(order);
-        m_ordersByUserId[order.userId()].insert(order.orderId());
+        m_ordersByUserId[order.user()].insert(order.orderId());
     }
 
     void cancelOrder(const std::string &orderId) override
     {
-        std::lock_guard<std::mutex> lock(m_mutex); // lock guard for thread safety
-
         auto orderit = m_ordersByOrderId.find(orderId);
         if (orderit != m_ordersByOrderId.end())
         {
             const auto &secId = orderit->second.securityId();
+            const auto &userId = orderit->second.user();
+
+            // remove from m_ordersBySecurityId
             auto &orders = m_ordersBySecurityId[secId];
             orders.erase(orderit->second);
             if (orders.empty())
                 m_ordersBySecurityId.erase(secId);
-            const auto &userId = orderit->second.userId();
+
+            // erase from m_ordersByUserId
             auto &userOrders = m_ordersByUserId[userId];
             userOrders.erase(orderId);
+
             if (userOrders.empty())
                 m_ordersByUserId.erase(userId);
+
             m_ordersByOrderId.erase(orderit);
+            std::cout << "order cancelled for orderid: " << orderId << std::endl;
         }
     }
+    std::optional<Order> getOrder(const std::string &orderId) const override
+    {
+        auto orderFound = m_ordersByOrderId.find(orderId);
+        if (orderFound != m_ordersByOrderId.end())
+            return orderFound->second;
+        return std::nullopt;
+    }
+
+    void cancelOrderByUser(const std::string &userId) override
+    {
+        std::lock_guard<std::mutex> lock(m_mutex); // lock guard for thread safety
+        auto userit = m_ordersByUserId.find(userId);
+        if (userit != m_ordersByUserId.end())
+        {
+            // delete all the order ids associated with this user
+            auto orders = userit->second;
+            for (const auto &orderId : orders)
+                cancelOrder(orderId);
+        }
+    }
+
+    std::vector<Order> getOrdersByUser(const std::string &userId) const override
+    {
+        std::lock_guard<std::mutex> lock(m_mutex); // lock guard for thread safety
+        std::vector<Order> orders;
+        auto orderit = m_ordersByUserId.find(userId);
+        if (orderit != m_ordersByUserId.end())
+        {
+            auto &orderIds = orderit->second;
+            for (const auto &orderId : orderIds)
+            {
+                const auto &optionalOrder = getOrder(orderId);
+                if (optionalOrder)
+                    orders.push_back(*optionalOrder);
+            }
+        }
+        return orders;
+    }
+
+    std::vector<Order> getAllOrders() const override
+    {
+
+        std::vector<Order> orders;
+        for (auto &[orderId, order] : m_ordersByOrderId)
+            orders.push_back(order);
+
+        return orders;
+    }
+
+    void cancelOrdersForSecIdWithMinimumQty(const std::string &securityId, unsigned int minQty) override
+    {
+        std::lock_guard<std::mutex> lock(m_mutex); // lock guard for thread safety
+        auto secit = m_ordersBySecurityId.find(securityId);
+        if (secit != m_ordersBySecurityId.end())
+        {
+            auto orders = secit->second;
+            for (const auto &order : orders)
+            {
+                if (order.qty() >= minQty)
+                    cancelOrder(order.orderId());
+            }
+        }
+    }
+
+    unsigned int getMatchingSizeForSecurity(const std::string &securityId) override
+    {
+        return 0;
+    }
+
+    // allows to modify existing order
+    void modifyOrder(const std::string &orderId, const unsigned int &newQty) override
+    {
+        auto orderit = m_ordersByOrderId.find(orderId);
+        if (orderit == m_ordersByOrderId.end())
+            throw std::runtime_error("Order with OrderId " + orderId + " doesn't exist");
+        auto oldOrder = orderit->second;
+        cancelOrder(orderId);
+        addOrder(Order(oldOrder.orderId(),
+                       oldOrder.securityId(),
+                       oldOrder.side(),
+                       newQty,
+                       oldOrder.user(),
+                       oldOrder.company(),
+                       oldOrder.price()));
+    }
 };
+
+template <typename T>
+void displayOrders(const T &container)
+{
+    for (const Order &order : container)
+    {
+        std::cout << "Order( " + order.orderId() + ", ";
+        std::cout << order.securityId() + ", " + order.user();
+        std::cout << ", " << order.qty() << ", " << order.company() << ", " << order.price() << " ) " << std::endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -4,8 +4,39 @@
 int main()
 {
     OrderEngine engine;
-    engine.addOrder(Order("ID1", "SecID1", "Buy", 100, "raju", "boa"));
-    engine.addOrder(Order("ID2", "SecID1", "Sell", 1000, "raju", "boa"));
+    engine.addOrder(Order("ID1", "SecID1", "Buy", 100, "raju", "boa", 50));
+    engine.addOrder(Order("ID2", "SecID1", "Sell", 1000, "raju", "boa", 30));
+    engine.addOrder(Order("ID3", "SecID2", "Buy", 500, "prema", "rush", 10));
+    engine.addOrder(Order("ID4", "SecID3", "Buy", 600, "prema", "rush", 55.5));
+    engine.addOrder(Order("ID5", "SecID4", "Buy", 300, "raju", "boa", 56.0));
+    engine.addOrder(Order("ID6", "SecID2", "Sell", 400, "neptune", "kernel"));
+    engine.addOrder(Order("ID7", "SecID3", "Sell", 500, "neptune", "kernel"));
+    engine.addOrder(Order("ID8", "SecID1", "Sell", 300, "neptune", "kernel", 66.7));
+    engine.addOrder(Order("ID9", "SecID2", "Buy", 500, "neptune", "kernel", 45.6));
+
+    auto allOrders = engine.getAllOrders();
+    std::cout << "There are a total of " << allOrders.size() << " Orders" << std::endl;
+    displayOrders(allOrders);
+    std::cout << std::endl;
+    std::cout << "Getting all Orders by raju" << std::endl;
+    auto orders = engine.getOrdersByUser("raju");
+    if (!orders.empty())
+        std::cout << "Total Orders by Raju: " << orders.size() << std::endl;
+    displayOrders(orders);
+    std::cout << "cancelling orders by raju" << std::endl;
+    engine.cancelOrderByUser("raju");
+    orders = engine.getOrdersByUser("raju");
+    if (!orders.empty())
+        std::cout << "Total Orders by Raju: " << orders.size() << std::endl;
+    else
+        std::cout << "No Orders by raju" << std::endl;
+    allOrders = engine.getAllOrders();
+    displayOrders(allOrders);
+    std::cout << "cancelng orders with secid2" << std::endl;
+    std::cout << "modifying order for ID9" << std::endl;
+    engine.modifyOrder("ID9", 100);
+    allOrders = engine.getAllOrders();
+    displayOrders(allOrders);
 
     return 0;
 }
